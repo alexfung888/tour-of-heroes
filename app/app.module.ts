4afe95a63162c53289f4cbd22fae644c5972a666
alexfung888 <1.alexfung@gmail.com>
2016-11-16 16:52:52 +0800

    Tutorial Chapter 6 Part 4: Refactor routes to app-routing.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,37 +1,17 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }     from './dashboard.component';
-import { HeroService }         from './hero.service';
-import { RouterModule }   from '@angular/router';
-
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+import { AppRoutingModule }     from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-      
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -39,12 +19,7 @@ import { RouterModule }   from '@angular/router';
     HeroDetailComponent,
     HeroesComponent
   ],
-
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-
-export class AppModule {
-}
+export class AppModule { }
