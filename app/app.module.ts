8f06ca5524abcce350857f72628ddb3985b6ddb3
alexfung888 <1.alexfung@gmail.com>
2016-11-16 14:52:03 +0800

    Tutorial Chapter 6 Part 2: Dashboard top heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,21 +4,42 @@ import { FormsModule }    from '@angular/forms';
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
 import { HeroService }         from './hero.service';
+import { RouterModule }   from '@angular/router';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
+
   providers: [
     HeroService
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {
 }
